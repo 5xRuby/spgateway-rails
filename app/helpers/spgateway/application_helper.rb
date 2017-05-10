f329fbb995d4383cd66ea80b2cf29120d437f0b9
zetavg <mail@zeta.vg>
2017-05-10 19:23:15 +0800

    build the spgateway_pay_button helper

--- app/helpers/spgateway/application_helper.rb
+++ app/helpers/spgateway/application_helper.rb
@@ -25,5 +25,34 @@ module Spgateway
         concat submit_tag submit, class: submit_class
       end
     end
+
+    def spgateway_pay_button(title, options)
+      raise ArgumentError, "Missing required argument: order_number." unless options[:order_number]
+      raise ArgumentError, "Missing required argument: item_description." unless options[:item_description]
+      raise ArgumentError, "Missing required argument: amount." unless options[:amount]
+
+      form_attributes = options.except(
+        :order_number,
+        :item_description,
+        :amount,
+        :class,
+        :return_url,
+        :mpg_gateway_url
+      )
+
+      form_attributes[:MerchantOrderNo] = options[:order_number]
+      form_attributes[:ItemDesc] = options[:item_description]
+      form_attributes[:Amt] = options[:amount]
+
+      form = Spgateway::MPGForm.new(form_attributes)
+
+      spgateway_mpg_form_for(
+        form,
+        submit: title,
+        submit_class: options[:class],
+        return_url: options[:return_url],
+        mpg_gateway_url: options[:mpg_gateway_url] || Spgateway.config.mpg_gateway_url
+      )
+    end
   end
 end
