7e46a1d14c5403bb541c41349bee5db7e3c7c911
zetavg <mail@zeta.vg>
2017-05-11 12:37:16 +0800

    remove unnecessary parts
    - Assets.
    - Mailers.
    - Models.
    - Jobs are retained because we may want to have payment checking jobs in the future.

--- app/mailers/spgateway/rails/application_mailer.rb
+++
@@ -1,8 +0,0 @@
-module Spgateway
-  module Rails
-    class ApplicationMailer < ActionMailer::Base
-      default from: 'from@example.com'
-      layout 'mailer'
-    end
-  end
-end
--- app/models/spgateway/rails/application_record.rb
+++
@@ -1,7 +0,0 @@
-module Spgateway
-  module Rails
-    class ApplicationRecord < ActiveRecord::Base
-      self.abstract_class = true
-    end
-  end
-end
