e97c38c68da51818c2ec724fb7cde9b5ef43f46f
zetavg <mail@zeta.vg>
2017-07-07 14:29:04 +0800

    resolves #2

--- app/controllers/spgateway/mpg_callbacks_controller.rb
+++ app/controllers/spgateway/mpg_callbacks_controller.rb
@@ -3,6 +3,8 @@ require_dependency 'spgateway/application_controller'
 
 module Spgateway
   class MPGCallbacksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def proceed
       raise NotImplementedError, 'Spgateway.config.mpg_callback is not a proc.' unless Spgateway.config.mpg_callback.is_a? Proc
       Spgateway.config.mpg_callback.call(spgateway_response, self, ::Rails.application.routes.url_helpers)
--- app/controllers/spgateway/payment_code_callbacks_controller.rb
+++ app/controllers/spgateway/payment_code_callbacks_controller.rb
@@ -3,6 +3,8 @@ require_dependency 'spgateway/application_controller'
 
 module Spgateway
   class PaymentCodeCallbacksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def proceed
       raise NotImplementedError, 'Spgateway.config.payment_code_callback is not a proc.' unless Spgateway.config.payment_code_callback.is_a? Proc
       Spgateway.config.payment_code_callback.call(spgateway_response, self, ::Rails.application.routes.url_helpers)
